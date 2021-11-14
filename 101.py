7891cc046f56d9e6ea73fc01f6bb089745526913
BitanKundu <79818809+BitanKundu@users.noreply.github.com>
2021-11-14 09:21:43 -0800

    Add files via upload

---
+++ 101.py
@@ -0,0 +1,16 @@
+
+for i in range(1,11):
+    print(i)
+
+
+for i in range(1,11)
+print(i)
+for i in range(1,21):
+    print(i)
+
+for i in range (1,21)
+    print(d)
+for i in range(1,51):
+    print(i)
+for d in range(1,51):
+    print(i)
